49e634094b8818ec16aab7e426bf30efa8f74f6b
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-23 21:08:28 -0400

    updated savedBooks

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -10,37 +10,13 @@ import {
 import { getMe, deleteBook } from '../utils/API'; // reFactor to use mutation + query for getSingleUser and deleteBook
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import { QUERY_ME } from '../utils/queries';
+import { useQuery } from '@apollo/client';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  const {loading, data } = useQuery(QUERY_ME)
+  const userData = data?.getSingleUser || {}
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -67,7 +43,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,17 @@
 // Implement the getSingleUser query
+import { gql } from"@apollo/client"
+export const QUERY_ME = gql`
+query SavedBooks {
+    getSingleUser {
+      savedBooks {
+        _id
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`
