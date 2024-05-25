bc280484c4451e9a7803ecf40b319bf9d17e8426
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-25 01:33:19 -0400

    reinstalled mongoose and updated connection.js to resolve database errors

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,9 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+}
+
+run().catch(console.dir);
 
 module.exports = mongoose.connection;
