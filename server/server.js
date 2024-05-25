a405bd00b182917daec8490123b6d0393ee23c63
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-25 02:10:24 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ async function startApolloServer() {
     });
   }
 
-  db.on('connected', () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
