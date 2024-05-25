66e7e5177d0e0e12dbf4ec5361eabc95689c3a18
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-25 00:21:21 -0400

    updated connection.js and added the missing .env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
